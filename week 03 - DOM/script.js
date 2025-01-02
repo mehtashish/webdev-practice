36ce0f56ef0082945052dcf82b683b85f767b6d0
mehtashish <mehta.mtj@gmail.com>
2025-01-02 22:48:50 +0530

    added deleteToDo()

--- week 03 - DOM/script.js
+++ week 03 - DOM/script.js
@@ -5,8 +5,14 @@ function addToDo() {
     const value = newEl.value;
 
     const newDivEL = document.createElement("div");
-    newDivEL.innerHTML = index + ". " + value;
+    newDivEL.setAttribute("id", "todo-" + index);
+    newDivEL.innerHTML = "<div>" + index + ". " + value + "</div><button onclick='deleteToDo(" + index + ")'>delete</button";
 
     document.querySelector("body").appendChild(newDivEL);
     index++;
+}
+
+function deleteToDo(index) {
+    const element = document.getElementById("todo-" + index);
+    element.parentNode.removeChild(element);
 }
